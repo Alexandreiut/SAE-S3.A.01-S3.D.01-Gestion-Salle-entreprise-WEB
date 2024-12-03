a8020b01d5bf4f52c959a648d9c1f81b22b338f3
Arcier Noé <168074918+NoeArcier@users.noreply.github.com>
2024-12-03 17:15:50 +0000

    creation fin CREATE

--- SQL_scripts/creationTables.sql
+++ SQL_scripts/creationTables.sql
@@ -13,7 +13,6 @@ CREATE TABLE `salle` (
   `ecranXXL` varchar(3) NOT NULL,
   `nombreOrdinateur` int(5) NOT NULL,
   `typeOrdinateur` varchar(70) NOT NULL,
-  `logicielPresent` varchar(5000) NOT NULL,
   `imprimante` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -22,24 +21,29 @@ CREATE TABLE `utilisateur` (
   `nom` varchar(70) NOT NULL,
   `prenom` varchar(70) NOT NULL,
   `telephone` char(10) NOT NULL,
-  `administrateur` varchar(3) NOT NULL,
+  `role` varchar(70) NOT NULL,
   `login` varchar(70) NOT NULL,
   `motDePasse` varchar(70) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `interlocuteur` (
+  `identifiant` int(7) NOT NULL,
+  `nom` varchar(70) NOT NULL,
+  `prenom` varchar(70) NOT NULL,
+  `telephone` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `reservation` (
   `identifiant` int(7) NOT NULL,
   `date` DATE NOT NULL,
-  `heureDebut` char(5) NOT NULL,
-  `heureFin` char(5) NOT NULL,
-  `descriptionActivite` char(100),
-  `descriptionActivite` char(100),
-  `descriptionActivite` char(100),
-  `descriptionActivite` char(100),
-  `descriptionActivite` char(100),
-  `descriptionActivite` char(100),
-  `descriptionActivite` char(100),
-  `descriptionActivite` char(100),
+  `heureDebut` TIME NOT NULL,
+  `heureFin` TIME NOT NULL,
+  `descriptionActivite` varchar(100),
+  `usage` varchar(70),
+  `interlocuteur` int(7),
+  `salle` int(8) NOT NULL,
+  `activite` int(7) NOT NULL,
+  `reservant` int(7) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -52,6 +56,9 @@ ALTER TABLE `salle`
   ADD PRIMARY KEY (`identifiant`);
 
 ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`identifiant`);
+
+ALTER TABLE `interlocuteur`
   ADD PRIMARY KEY (`identifiant`);
 
 ALTER TABLE `reservation`
