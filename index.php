da351f0a82d2d1372b174bfaa9abce84b0de3f31
NOE ARCIER <noe.arcier@iut.local>
2024-12-06 13:36:01 +0100

    Debut PHP

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <a href = "pages/accueil.php" target = "_blank" title "accueil">accueil</a><br>
+        <a href = "pages/aide.php" target = "_blank" title "aide">aide</a><br>
+        <a href = "pages/connexion.php" target = "_blank" title "connexion">connexion</a><br>
+    </body>
+</html>
