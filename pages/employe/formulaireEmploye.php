cab2b78f67653baeca40b3c8c6138fec5524a7b0
NOE ARCIER <noe.arcier@iut.local>
2024-12-06 14:31:58 +0100

    mise en place création

---
+++ pages/employe/formulaireEmploye.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>RoomManager - Création employés</title>
+        <meta name="Description" content="" />
+        <link rel="stylesheet" href="../../ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../../ressources/fontawesome-free-6.5.1-web/css/all.css">
+        <link rel="stylesheet" href="../../css/creation.css" />
+    </head>
+    <body>
+        OUI
+    </body>
+</html>    
