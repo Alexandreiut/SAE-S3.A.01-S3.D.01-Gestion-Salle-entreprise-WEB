7890bac25f426199700f8d6658247416377931ce
ADRIEN ASTIER <adrien.astier@iut.local>
2024-12-06 14:38:24 +0100

    Vue Connexion

--- index.php
+++ index.php
@@ -2,32 +2,33 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/connexion.css" />
 		<link rel="stylesheet" href="ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
 		<link rel="stylesheet" href="ressources/fontawesome-free-6.5.1-web/css/all.min.css">
 		<title>Connexion</title>
 		
 	</head>
 	<body>
-		<div class="container">
+		<div>
+			<img src="ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
 			<div class="header">
-				<h1>Connexion</h1>
+    			<h1>Connexion</h1>
 			</div>
-			<form method="post" action="index.php">
-				<h2>Accéder à mon compte</h2>
-				<div>
-					<label for="login">Identifiant :</label>
-					<input type="text" id="login" name="login" required>
-				</div>
-				<div>
-					<label for="pwd">Mot de Passe :</label>
-					<input type="password" id="pwd" name="pwd" required>
-				</div>
-				<button type="submit" class="btn">Se connecter</button>
-			</form>
-			<div class="footer">
-				<p>2024 © RoomManager. IUT de Rodez.</p>
+		</div>
+		<form method="post" action="index.php">
+			<h2>Accéder à mon compte</h2>
+			<div>
+				<label for="login">Identifiant :</label>
+				<input type="text" id="login" name="login" required>
 			</div>
+			<div>
+				<label for="pwd">Mot de Passe :</label>
+				<input type="password" id="pwd" name="pwd" required>
+			</div>
+			<button type="submit" class="btn">Se connecter</button>
+		</form>
+		<div class="footer">
+			<p>2024 © RoomManager. IUT de Rodez.</p>
 		</div>
 	</body>
 </html>
