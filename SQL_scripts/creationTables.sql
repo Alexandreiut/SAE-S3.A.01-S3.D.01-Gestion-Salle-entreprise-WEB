97d326ec05f544ccc5874e6aa8f7ab3741004ea2
Alexandreiut <131276833+Alexandreiut@users.noreply.github.com>
2024-12-06 11:40:22 +0100

    Update creationTables.sql correction

--- SQL_scripts/creationTables.sql
+++ SQL_scripts/creationTables.sql
@@ -117,7 +117,7 @@ ALTER TABLE `reservation`
 
 ALTER TABLE `logiciel_salle`
   ADD CONSTRAINT `fk_logiciel` FOREIGN KEY (`logiciel`) REFERENCES `logiciel` (`identifiant`),
-  ADD CONSTRAINT `fk_salle` FOREIGN KEY (`salle`) REFERENCES `salle` (`identifiant`),
+  ADD CONSTRAINT `fk_salle` FOREIGN KEY (`salle`) REFERENCES `salle` (`identifiant`);
 
 ALTER TABLE `reservation`
   ADD CONSTRAINT `fk_interlocuteur` FOREIGN KEY (`interlocuteur`) REFERENCES `interlocuteur` (`identifiant`),
