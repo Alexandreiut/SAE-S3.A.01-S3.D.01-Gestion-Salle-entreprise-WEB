9113d65876408ee4b1ba492c66141d94f41d6954
NOE ARCIER <noe.arcier@iut.local>
2024-12-06 14:03:26 +0100

    attente création branche consultation

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <a href = "pages/accueil.php" target = "_blank" title "accueil">accueil</a><br>
-        <a href = "pages/aide.php" target = "_blank" title "aide">aide</a><br>
+        <a href = "pages/exportation.php" target = "_blank" title "exportation">exportation</a><br>
         <a href = "pages/connexion.php" target = "_blank" title "connexion">connexion</a><br>
     </body>
 </html>
---
+++ pages/employe/consultationEmploye.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>RoomManager - Consultation employés</title>
+        <meta name="Description" content="" />
+        <link rel="stylesheet" href="../outils/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../outils/fontawesome-free-6.5.1-web/css/all.css">
+        <link rel="stylesheet" href="../css/style.css" />
+    </head>
+    <body>
+        OUI
+    </body>
+</html>    
