ae82b812e1d72e14f25b43a8a9f578219539deba
NOE ARCIER <noe.arcier@iut.local>
2024-12-06 14:22:57 +0100

    mise en place consultation

--- pages/employe/consultationEmploye.php
+++ pages/employe/consultationEmploye.php
@@ -3,9 +3,9 @@
     <head>
         <title>RoomManager - Consultation employés</title>
         <meta name="Description" content="" />
-        <link rel="stylesheet" href="../outils/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="../outils/fontawesome-free-6.5.1-web/css/all.css">
-        <link rel="stylesheet" href="../css/style.css" />
+        <link rel="stylesheet" href="../../ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../../ressources/fontawesome-free-6.5.1-web/css/all.css">
+        <link rel="stylesheet" href="../../css/consultation.css" />
     </head>
     <body>
         OUI
