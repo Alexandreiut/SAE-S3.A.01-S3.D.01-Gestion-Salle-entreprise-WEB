29de42a8e63ceb39ecf65c2cbd66d0f0845ac1ce
ALEXANDRE BROUZES <alexandre.brouzes@iut.local>
2024-12-09 13:31:19 +0100

    jeu de donnée ok

--- SQL_scripts/creationTables.sql
+++ SQL_scripts/creationTables.sql
@@ -1,21 +1,26 @@
--- CREATION BASE DE DONNEES
-CREATE DATABASE IF NOT EXISTS RoomManager DEFAULT CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci;
-USE RoomManager;
+-- SUPPRESSION DES TABLES EXISTANTES (SI ELLES EXISTENT)
+DROP TABLE IF EXISTS `reservation`;
+DROP TABLE IF EXISTS `logiciel_salle`;
+DROP TABLE IF EXISTS `salle`;
+DROP TABLE IF EXISTS `interlocuteur`;
+DROP TABLE IF EXISTS `utilisateur`;
+DROP TABLE IF EXISTS `logiciel`;
+DROP TABLE IF EXISTS `activite`;
 
 -- CREATION TABLES
 
 CREATE TABLE `activite` (
   `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `logiciel` (
   `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `salle` (
-  `identifiant` int(8) NOT NULL,
+  `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL,
   `capacite` int(5) NOT NULL,
   `videoProjecteur` varchar(3) NOT NULL,
@@ -23,12 +28,12 @@ CREATE TABLE `salle` (
   `nombreOrdinateur` int(5) NOT NULL,
   `typeOrdinateur` varchar(70) NOT NULL,
   `imprimante` varchar(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `logiciel_salle` (
   `id_logiciel` int(7) NOT NULL,
   `id_salle` int(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `utilisateur` (
   `identifiant` int(7) NOT NULL,
@@ -38,14 +43,14 @@ CREATE TABLE `utilisateur` (
   `role` varchar(70) NOT NULL,
   `login` varchar(70) NOT NULL,
   `motDePasse` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `interlocuteur` (
-  `identifiant` int(7) NOT NULL,
+  `identifiant` int(7) NOT NULL,zz
   `nom` varchar(70) NOT NULL,
   `prenom` varchar(70) NOT NULL,
   `telephone` char(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `reservation` (
   `identifiant` int(7) NOT NULL,
@@ -53,13 +58,12 @@ CREATE TABLE `reservation` (
   `heureDebut` TIME NOT NULL,
   `heureFin` TIME NOT NULL,
   `descriptionActivite` varchar(100),
-  `usage` varchar(70),
+  `object` varchar(70),
   `interlocuteur` int(7),
-  `salle` int(8) NOT NULL,
+  `salle` int(7) NOT NULL,
   `activite` int(7) NOT NULL,
   `reservant` int(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- PRIMARY KEYS
 
@@ -88,7 +92,6 @@ ALTER TABLE `reservation`
   ADD KEY `fk_activite` (`activite`),
   ADD KEY `fk_utilisateur` (`reservant`);
 
-
 -- AUTO INCREMENTS
 
 ALTER TABLE `activite`
@@ -109,12 +112,11 @@ ALTER TABLE `interlocuteur`
 ALTER TABLE `reservation`
   MODIFY `identifiant` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
-
 -- CONSTRAINTS
 
 ALTER TABLE `logiciel_salle`
-  ADD CONSTRAINT `fk_logiciel` FOREIGN KEY (`logiciel`) REFERENCES `logiciel` (`identifiant`),
-  ADD CONSTRAINT `fk_salle` FOREIGN KEY (`salle`) REFERENCES `salle` (`identifiant`),
+  ADD CONSTRAINT `fk_logiciel` FOREIGN KEY (`id_logiciel`) REFERENCES `logiciel` (`identifiant`),
+  ADD CONSTRAINT `fk_salle` FOREIGN KEY (`id_salle`) REFERENCES `salle` (`identifiant`);
 
 ALTER TABLE `reservation`
   ADD CONSTRAINT `fk_interlocuteur` FOREIGN KEY (`interlocuteur`) REFERENCES `interlocuteur` (`identifiant`),
