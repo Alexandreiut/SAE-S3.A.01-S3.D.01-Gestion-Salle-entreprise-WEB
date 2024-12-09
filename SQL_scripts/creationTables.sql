06040ee8ea48f3607bab75e16d962d6e18d63bb6
Alexandreiut <131276833+Alexandreiut@users.noreply.github.com>
2024-12-09 11:55:59 +0100

    update

--- SQL_scripts/creationTables.sql
+++ SQL_scripts/creationTables.sql
@@ -1,29 +1,26 @@
--- CREATION BASE DE DONNEES
-CREATE DATABASE IF NOT EXISTS RoomManager DEFAULT CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci;
-USE RoomManager;
-
--- SUPPRESSION DES TABLES
-DROP table if EXISTS reservation;
-DROP table if EXISTS logiciel_salle;
-DROP table if EXISTS interlocuteur;
-DROP table if EXISTS utilisateur;
-DROP table if EXISTS activite;
-DROP table if EXISTS logiciel;
-DROP table if EXISTS salle;
+-- SUPPRESSION DES TABLES EXISTANTES (SI ELLES EXISTENT)
+DROP TABLE IF EXISTS `reservation`;
+DROP TABLE IF EXISTS `logiciel_salle`;
+DROP TABLE IF EXISTS `salle`;
+DROP TABLE IF EXISTS `interlocuteur`;
+DROP TABLE IF EXISTS `utilisateur`;
+DROP TABLE IF EXISTS `logiciel`;
+DROP TABLE IF EXISTS `activite`;
 
 -- CREATION TABLES
+
 CREATE TABLE `activite` (
   `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `logiciel` (
   `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `salle` (
-  `identifiant` int(8) NOT NULL,
+  `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL,
   `capacite` int(5) NOT NULL,
   `videoProjecteur` varchar(3) NOT NULL,
@@ -31,12 +28,12 @@ CREATE TABLE `salle` (
   `nombreOrdinateur` int(5) NOT NULL,
   `typeOrdinateur` varchar(70) NOT NULL,
   `imprimante` varchar(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `logiciel_salle` (
   `id_logiciel` int(7) NOT NULL,
   `id_salle` int(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `utilisateur` (
   `identifiant` int(7) NOT NULL,
@@ -46,14 +43,14 @@ CREATE TABLE `utilisateur` (
   `role` varchar(70) NOT NULL,
   `login` varchar(70) NOT NULL,
   `motDePasse` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `interlocuteur` (
   `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL,
   `prenom` varchar(70) NOT NULL,
   `telephone` char(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `reservation` (
   `identifiant` int(7) NOT NULL,
@@ -61,13 +58,12 @@ CREATE TABLE `reservation` (
   `heureDebut` TIME NOT NULL,
   `heureFin` TIME NOT NULL,
   `descriptionActivite` varchar(100),
-  `usage` varchar(70),
+  `object` varchar(70),
   `interlocuteur` int(7),
-  `salle` int(8) NOT NULL,
+  `salle` int(7) NOT NULL,
   `activite` int(7) NOT NULL,
   `reservant` int(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- PRIMARY KEYS
 
@@ -96,7 +92,6 @@ ALTER TABLE `reservation`
   ADD KEY `fk_activite` (`activite`),
   ADD KEY `fk_utilisateur` (`reservant`);
 
-
 -- AUTO INCREMENTS
 
 ALTER TABLE `activite`
@@ -116,7 +111,6 @@ ALTER TABLE `interlocuteur`
 
 ALTER TABLE `reservation`
   MODIFY `identifiant` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-
 
 -- CONSTRAINTS
 
--- SQL_scripts/jeu_de_donnees.sql
+++ SQL_scripts/jeu_de_donnees.sql
@@ -10,13 +10,8 @@ VALUES
 
 -- Utilisateur
 
--- Administrateur
-INSERT INTO `utilisateur` (nom, prenom, telephone, role, login, motDePasse)
-VALUES
-('nomAdmin', 'prenomAdmin', '2614000000', 'administrateur', 'admin', 'password123');
-
 -- Employés
-INSERT INTO `utilisateur` (nom, prenom, telephone, role, login, motDePasse)
+INSERT INTO utilisateur (nom, prenom, telephone, role, login, motDePasse)
 VALUES
 ('Dupont', 'Pierre', '2614000000', 'employé', 'pierre_dupont', 'password123'),
 ('Lexpert', 'Noemie', '2614000000', 'employé', 'noemie_lexpert', 'password123'),
@@ -27,8 +22,13 @@ VALUES
 ('Legrand', 'Jean-Pierre', '2689000000', 'employé', 'jean_pierre_legrand', 'password123'),
 ('Deneuve', 'Zoé', '2626000000', 'employé', 'zoe_denueve', 'password123');
 
+-- Administrateur
+INSERT INTO utilisateur (nom, prenom, telephone, role, login, motDePasse)
+VALUES
+('nomAdmin', 'prenomAdmin', '2614000000', 'administrateur', 'admin', 'password123');
+
 -- Logiciels 
-INSERT INTO `logiciel` (nom) 
+INSERT INTO logiciel (nom) 
 VALUES 
 ('bureautique'),
 ('java'),
@@ -36,20 +36,20 @@ VALUES
 ('photoshop');
 
 -- Salles 
-INSERT INTO `salle` (nom, capacite, videoProjecteur, ecranXXL, nombreOrdinateur, typeOrdinateur, imprimante) 
+INSERT INTO salle (nom, capacite, videoProjecteur, ecranXXL, nombreOrdinateur, typeOrdinateur, imprimante) 
 VALUES 
 ('A6', 15, 'oui', 'non', 4, 'PC portable', 'non'),
-('salle bleue', 18, 'oui', 'oui', NULL, NULL, NULL),
-('salle ronde', 14, 'oui', 'non', NULL, NULL, NULL),
-('salle Picasso', 15, 'non', 'non', NULL, NULL, NULL),
-('petite salle', 7, 'oui', 'oui', NULL, NULL, NULL),
-('A7', 4, 'non', 'non', NULL, NULL, NULL),
-('salle patio', 6, 'non', 'non', NULL, NULL, NULL),
+('salle bleue', 18, 'oui', 'oui', NULL, NULL, 'non'),
+('salle ronde', 14, 'oui', 'non', NULL, NULL, 'non'),
+('salle Picasso', 15, 'non', 'non', NULL, NULL, 'non'),
+('petite salle', 7, 'oui', 'oui', NULL, NULL, 'non'),
+('A7', 4, 'non', 'non', NULL, NULL, 'non'),
+('salle patio', 6, 'non', 'non', NULL, NULL, 'non'),
 ('salle Sydney', 20, 'oui', 'non', 16, 'PC Windows', 'non'),
 ('salle Brisbane', 22, 'oui', 'non', 18, 'PC Windows', 'oui');
 
 -- logiciel - salle
-INSERT INTO `logiciel_salle` (id_logiciel, id_salle) 
+INSERT INTO logiciel_salle (id_logiciel, id_salle) 
 VALUES 
 (1, 1),  -- Logiciel 1 (bureautique) dans salle 1 (A6)
 (1, 8),  -- Logiciel 1 (bureautique) dans salle 8 (salle Sydney)
@@ -59,4 +59,34 @@ VALUES
 (3, 8),  -- Logiciel 3 (Intellij) dans salle 8 (salle Sydney)
 (3, 9),  -- Logiciel 3 (Intellij) dans salle 9 (salle Brisbane)
 (4, 9);  -- Logiciel 4 (photoshop) dans salle 9 (salle Brisbane)
+
+-- interlocuteurs
+INSERT INTO interlocuteur (nom, prenom, telephone) 
+VALUES 
+('Legendre', 'Noémie', '0600000000'),
+('Leroux', 'Jacques', '0600000001'),
+('Marin', 'Hector', '0666666666'),
+('Tournefeuille', 'Michel', '0655555555');
+
+-- Réservations
+INSERT INTO reservation (date, heureDebut, heureFin, descriptionActivite, object, interlocuteur, salle, activite, reservant)
+VALUES 
+('2024-10-07', '17:00', '19:00', 'club gym', 'réunion', 1, 1, 4, 1),
+('2024-10-07', '15:00', '18:00', 'réunion avec client', '', NULL, 4, 1, 1),
+('2024-10-07', '10:00', '11:00', 'Préparation réunion client', '', NULL, 4, 1, 5),
+('2024-10-08', '09:00', '11:00', '', '', NULL, 4, 1, 2),
+('2024-10-08', '17:00', '19:00', 'club gym', 'AG', 1, 1, 4, 3),
+('2024-10-09', '09:00', '12:00', 'tests candidats', '', NULL, 8, 6, 7),
+('2024-10-07', '15:00', '18:00', 'présentation maquette', '', NULL, 3, 1, 7),
+('2024-10-10', '08:00', '18:00', 'Bureautique', '', 2, 1, 2, 3),
+('2024-10-11', '08:00', '18:00', 'Bureautique', '', 2, 1, 2, 3),
+('2024-10-07', '10:00', '12:00', 'accueil nouveau membre', '', NULL, 3, 1, 8),
+('2024-10-10', '09:00', '12:00', 'tests candidats', '', NULL, 8, 6, 1),
+('2024-10-15', '09:00', '10:00', 'point avec stagiaire', '', NULL, 6, 1, 7),
+('2024-10-11', '08:00', '17:00', 'mise à jour logiciels', '', NULL, 8, 3, 3),
+('2024-10-11', '08:00', '17:00', 'mise à jour logiciels', '', NULL, 9, 3, 7),
+('2024-10-16', '10:00', '11:00', 'visite tuteur IUT', '', NULL, 6, 1, 7),
+('2024-10-17', '14:00', '15:30', 'validation maquette', '', NULL, 6, 1, 5),
+('2024-10-18', '08:00', '13:00', 'Mairie', 'réunion', 3, 1, 5, 8),
+('2024-10-18', '13:00', '19:00', 'Département', 'réunion', 4, 1, 5, 5);
 
