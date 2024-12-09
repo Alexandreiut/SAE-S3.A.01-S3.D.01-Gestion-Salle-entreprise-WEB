6853d646d3a569e2fcccb286800dcc0c6ce07db1
NOE ARCIER <noe.arcier@iut.local>
2024-12-09 17:13:54 +0100

    frontend formulaire ajout employés

--- pages/employe/formulaireEmploye.php
+++ pages/employe/formulaireEmploye.php
@@ -10,7 +10,37 @@
     <body>
         <div class = "header">
             <img src="../../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
-            <h1>oui<span class = "fas fa-user"></span></h1>
+            <h1><span class = "fas fa-plus"></span><span class = "fas fa-user"></span> Employé</h1>
         </div> 
+        <div class = "container">
+            <div class = "row">
+                <div class = "col-12">
+                    <form action = "" method = "post">
+                        <div class = "cadre_principal">
+                            <!-- nom -->
+                            <label for = "nom">Nom : </label>&emsp;
+                            <input type = "text" name = "nom" id = "nom" placeholder = "Dupond, Smith, ..."><br>
+                            
+                            <!-- prénom -->
+                            <label for = "prenom">Prénom : </label>&emsp;
+                            <input type = "text" name = "prenom" id = "prenom" placeholder = "Jean, Marie, ..."><br>
+                            
+                            <!-- téléphone -->
+                            <label for = "telephone">Téléphone : </label>&emsp;
+                            <input type = "text" name = "telephone" id = "telephone" placeholder = "0987654321"><br>
+                            
+                            <!-- login -->
+                            <label for = "login">Login : </label>&emsp;
+                            <input type = "text" name = "login" id = "login" placeholder = "hublublu, machin, ..."><br>
+                            
+                            <!-- mot de passe -->
+                            <label for = "mdp">Mot de Passe : </label>&emsp;
+                            <input type = "password" name = "mdp" id = "mdp" placeholder = "*****, ******* ..."><br>
+                        </div>
+                        <button type="submit" class="btn-ajouter"><span class = "fas fa-plus"></span><span class = "fas fa-user"></span> Ajouter l'employé</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>    
