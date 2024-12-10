82db59dfb728ea1e84cc322db14575a50962bafc
NOE ARCIER <noe.arcier@iut.local>
2024-12-10 09:46:39 +0100

    formulaire employe moitié

--- pages/employe/formulaireEmploye.php
+++ pages/employe/formulaireEmploye.php
@@ -17,27 +17,37 @@
                 <div class = "col-12">
                     <form action = "" method = "post">
                         <div class = "cadre_principal">
-                            <!-- nom -->
-                            <label for = "nom">Nom : </label>&emsp;
-                            <input type = "text" name = "nom" id = "nom" placeholder = "Dupond, Smith, ..."><br>
-                            
-                            <!-- prénom -->
-                            <label for = "prenom">Prénom : </label>&emsp;
-                            <input type = "text" name = "prenom" id = "prenom" placeholder = "Jean, Marie, ..."><br>
-                            
-                            <!-- téléphone -->
-                            <label for = "telephone">Téléphone : </label>&emsp;
-                            <input type = "text" name = "telephone" id = "telephone" placeholder = "0987654321"><br>
-                            
-                            <!-- login -->
-                            <label for = "login">Login : </label>&emsp;
-                            <input type = "text" name = "login" id = "login" placeholder = "hublublu, machin, ..."><br>
-                            
-                            <!-- mot de passe -->
-                            <label for = "mdp">Mot de Passe : </label>&emsp;
-                            <input type = "password" name = "mdp" id = "mdp" placeholder = "*****, ******* ..."><br>
+                            <div class = "row">
+                                <div class = "col-4">
+                                    <!-- nom -->
+                                    <label for = "nom">Nom : </label>
+                                    <input type = "text" name = "nom" id = "nom" placeholder = "Dupond, Smith, ...">
+                                </div>
+                                <div class = "col-4">
+                                    <!-- prénom -->
+                                    <label for = "prenom">Prénom : </label>
+                                    <input type = "text" name = "prenom" id = "prenom" placeholder = "Jean, Marie, ...">
+                                </div>
+                                <div class = "col-4">
+                                    <!-- téléphone -->
+                                    <label for = "telephone">Téléphone : </label>
+                                    <input type = "text" name = "telephone" id = "telephone" placeholder = "0987654321">
+                                </div>
+                                <div class = "col-2"></div>
+                                <div class = "col-4">
+                                    <!-- login -->
+                                    <label for = "login">Login : </label>
+                                    <input type = "text" name = "login" id = "login" placeholder = "hublublu, machin, ...">
+                                </div>
+                                <div class = "col-4">
+                                    <!-- mot de passe -->
+                                    <label for = "mdp">Mot de Passe : </label>
+                                    <input type = "password" name = "mdp" id = "mdp" placeholder = "*****, ******* ...">
+                                </div>
+                                <div class = "col-2"></div>
+                            </div>
                         </div>
-                        <button type="submit" class="btn-ajouter"><span class = "fas fa-plus"></span><span class = "fas fa-user"></span> Ajouter l'employé</button>
+                    <button type="submit" class="btn-ajouter"><span class = "fas fa-plus"></span><span class = "fas fa-user"></span> Ajouter l'employé</button>
                     </form>
                 </div>
             </div>
