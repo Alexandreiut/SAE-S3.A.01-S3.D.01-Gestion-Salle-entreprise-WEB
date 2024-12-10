a02405e08d466253b00adb266dd1187620e9fbdd
Astier Adrien <adrien.astier@gmail.com>
2024-12-10 12:45:48 +0100

    Commencement vue admin

--- pages/accueilAdmin.php
+++ pages/accueilAdmin.php
@@ -16,19 +16,25 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="css/connexion.css" />
-		<link rel="stylesheet" href="ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="ressources/fontawesome-free-6.5.1-web/css/all.min.css">
+		<link rel="stylesheet" href="../css/connexion.css" />
+		<link rel="stylesheet" href="../ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="../ressources/fontawesome-free-6.5.1-web/css/all.css">
 		<title>Accueil</title>
 		
 	</head>
 	<body>
-		Accueil Admin
-		<br/>
-		<form method="post" action="accueilAdmin.php">
-			<?php echo "<span class='login'>".$_SESSION['login']."</span>"; ?>
-			<input type="hidden" name="deconnexion" id="deconnexion" value="1">
-			<button type="submit" class="btn btn-danger mb-2 mt-2">Se déconnecter</button>
-		</form>
+		<div class="header">
+    		<img src="../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
+    		<h1>Accueil</h1>
+			<form method="post" action="accueilAdmin.php">
+				<input type="hidden" name="deconnexion" id="deconnexion" value="1">
+				<button type="submit" class="btn-deconnexion">Se déconnecter</button>
+			</form>
+		</div>
+
+		<div class="footer">
+			<p>2024 © RoomManager. IUT de Rodez.</p>
+		</div>
+		<script src="engine/js/oeil.js" defer></script>
 	</body>
 </html>
