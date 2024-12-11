58a911649ba26102812f03e86c74ab1af71a5c59
SamuelLacam <s.lacam46270@gmail.com>
2024-12-11 08:43:13 +0100

    bandeau des pages en commun terminé

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
+
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="css/consultations.css" />
-		<link rel="stylesheet" href="ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="ressources/fontawesome-free-6.5.1-web/css/all.min.css">
-		<title>Connexion</title>
-		
-	</head>
-	<body>
-		<div class="container">
-			<div class="header">
-				<h1>Connexion</h1>
-			</div>
-			<form method="post" action="index.php">
-				<h2>Accéder à mon compte</h2>
-				<div>
-					<label for="login">Identifiant :</label>
-					<input type="text" id="login" name="login" required>
-				</div>
-				<div>
-					<label for="pwd">Mot de Passe :</label>
-					<input type="password" id="pwd" name="pwd" required>
-				</div>
-				<button type="submit" class="btn">Se connecter</button>
-			</form>
-			<div class="footer">
-				<p>2024 © RoomManager. IUT de Rodez.</p>
-			</div>
-		</div>
-	</body>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/connexion.css" />
+    <link rel="stylesheet" href="ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="ressources/fontawesome-free-6.5.1-web/css/all.min.css">
+    <title>Connexion</title>
+
+</head>
+<body>
+<div class="header">
+    <img src="ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
+    <h1>Connexion</h1>
+</div>
+<form method="post" action="index.php">
+    <br/>
+    <h2 class="acces-text">Accéder à mon compte</h2>
+    <div>
+        <br/>
+        <label for="login" class="saisie-text">Identifiant :</label><br/>
+        <input type="text" id="login" name="login" required>
+    </div>
+    <div>
+        <label for="pwd" class="saisie-text">Mot de Passe :</label><br/>
+        <input type="password" id="pwd" name="pwd" required>
+    </div>
+    <br/>
+    <br/>
+    <button type="submit" class="btn-connexion">Se connecter</button>
+</form>
+<div class="footer">
+    <p>2024 © RoomManager. IUT de Rodez.</p>
+</div>
+</body>
 </html>
--- pages/employe/consultationEmploye.php
+++ pages/employe/consultationEmploye.php
@@ -4,10 +4,30 @@
         <title>RoomManager - Consultation employés</title>
         <meta name="Description" content="" />
         <link rel="stylesheet" href="../../ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="../../ressources/fontawesome-free-6.5.1-web/css/all.css">
+        <link rel="stylesheet" href="../../ressources/fontawesome-free-6.5.1-web/css/all.min.css">
         <link rel="stylesheet" href="../../css/consultations.css" />
     </head>
     <body>
-        OUI
-    </body>D
-</html>    
+        <!-- Barre de navigation -->
+        <div class="header">
+            <!-- Section gauche -->
+            <div class="header-left">
+                <i class="fa-solid fa-bars menu" alt="menu" id="menu"></i>
+                <img src="../../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
+            </div>
+
+            <!-- Titre au centre -->
+            <div class="header-title">
+                <i class="fa-solid fa-user"></i>
+                <h1>Employé</h1>
+            </div>
+
+            <!-- Bouton à droite -->
+            <button type="submit" class="deconnexion">
+                Se déconnecter
+                <i class="fa-solid fa-right-from-bracket"></i>
+            </button>
+        </div>
+
+    </body>
+</html>
