06c87502669737120668d39d4502bdcc1ccacf3a
LUCAS BOULOUARD <lucas.boulouard@iut.local>
2024-12-11 08:34:06 +0100

    début de la page exportation

---
+++ pages/exportation.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../ressources/fontawesome-free-6.5.1-web/css/all.min.css"> <!-- Lien vers Font Awesome -->
+        <link rel="stylesheet" href="../ressources/boostrap-5.3.2-dist/css/boostrap.min.css">
+        <link rel="stylesheet" href="../css/exportationCss.css">
+    </head>
+    <body>
+        <div class="header">
+            <img src="../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
+            <h1>Exportation</h1>
+        </div>
+
+        <div class="container">
+            <div class="acces-text">Sélectionner les éléments à exporter :</div>
+
+            <button class="btn">
+                <i class="fas fa-calendar-alt"></i> Exporter activités
+            </button>
+            <button class="btn">
+                <i class="fas fa-users"></i> Exporter employés
+            </button>
+            <button class="btn">
+                <i class="fas fa-cogs"></i> Exporter réservations
+            </button>
+            <button class="btn">
+                <i class="fas fa-store"></i> Exporter salles
+            </button>
+            <button class="btn">
+                <i class="fas fa-download"></i> Tout exporter
+            </button>
+        </div>
+
+        <footer class="footer">
+            © 2024 Room Manager | Assistance : support@roommanager.com
+        </footer>
+    </body>
+</html>
