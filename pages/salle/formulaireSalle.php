1b72870fea76db2983069c5a705b440a6bb64872
NOE ARCIER <noe.arcier@iut.local>
2024-12-11 08:39:52 +0100

    création salle même format qu employé

---
+++ pages/salle/formulaireSalle.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>RoomManager - Création salle</title>
+        <meta name="Description" content="" />
+        <link rel="stylesheet" href="../../ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../../ressources/fontawesome-free-6.5.1-web/css/all.css">
+        <link rel="stylesheet" href="../../css/creation.css" />
+    </head>
+    <body>
+        <div class = "header">
+            <img src="../../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
+            <h1><span class = "fas fa-plus"></span><span class = "fas fa-door-open"></span> Salle</h1>
+        </div> 
+        <div class = "container">
+            <form action = "" method = "post">
+                <div class = "row">
+                    <div class = "col-lg-6 col-12">
+                        <div class = "cadre_principal">
+                            <div class = "row">
+                                <div class = "col-12 titre">
+                                    OUI
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class = "col-lg-6 col-12">
+                        <div class = "cadre_principal col-6">
+                            <div class = "row">
+                                <div class = "col-12 titre">
+                                    OUI
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class = "col-6 offset-4">
+                        <button type="submit" class="btn-ajouter"><span class = "fas fa-plus"></span><span class = "fas fa-door-open"></span> Ajouter la salle</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>    
