f87cf60723cce60747583f03010e6fce7fb9c96d
NOE ARCIER <noe.arcier@iut.local>
2024-12-11 11:53:54 +0100

    formulaire employé insertion erreurs

---
+++ pages/employe/fonctions.php
@@ -0,0 +1,69 @@
+<?php
+    
+    function connexionBD() {
+        $host='localhost';
+        $db='roommanager';
+        $user='root';
+        $pass='root';
+        $charset='utf8mb4';
+        
+        $dsn="mysql:host=$host;dbname=$db;charset=$charset";
+        
+        $options=[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+                  PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+                  PDO::ATTR_EMULATE_PREPARES=>false];
+        
+        $pdo=new PDO($dsn,$user,$pass,$options);
+        
+        return $pdo;
+    }
+    
+    function ajoutEmploye($pdo, $nom, $prenom, $telephone, $login, $mdp) {
+        
+        $requete = "INSERT INTO utilisateur(nom, prenom, telephone, role, login, motDePasse)
+                    VALUES (:nom, :prenom, NULLIF(:telephone, ''), 'employe', :login, md5(:motDePasse))";
+        
+        $stmt = $pdo->prepare($requete);
+        
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':telephone', $telephone);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':motDePasse', $mdp);
+        
+        $stmt->execute();
+    }
+    
+    function verifChamps() {
+        
+        $verifications = [];
+        
+        if (isset($_POST['telephone']) && $_POST['telephone'] != "" ) {
+            
+            // vérifie que le téléphone soit un entier
+            try {
+                intval($_POST['telephone']);
+                $estEntier = true;
+            } catch (Exception $e) {
+                $estEntier = false;
+            }
+        }
+        
+        $verifications['nom'] = isset($_POST['nom']) && $_POST['nom'] != "";
+        $verifications['prenom'] = isset($_POST['prenom']) && $_POST['prenom'] != "";
+        $verifications['telephone'] = isset($_POST['telephone']) 
+                                      && ($_POST['telephone'] == ""
+                                          || strlen($_POST['telephone']) == 4
+                                          && $estEntier);
+        $verifications['login'] = isset($_POST['login']) && $_POST['login'] != "";
+        $verifications['mdp'] = isset($_POST['mdp']) && $_POST['mdp'] != "";
+        $verifications['confirm_mdp'] = isset($_POST['confirm_mdp']) && $_POST['confirm_mdp'] != "" && $_POST['confirm_mdp'] == $_POST['mdp'];
+        
+        $verifications['tout'] = true;
+        foreach ($verifications as $verif) {
+            $verifications['tout'] &= $verif;
+        }
+        
+        return $verifications;
+    }
+?>
--- pages/employe/formulaireEmploye.php
+++ pages/employe/formulaireEmploye.php
@@ -1,3 +1,34 @@
+<?php
+
+    session_start();
+    
+    try {
+        require('fonctions.php');
+        
+        if (!empty($_POST)) {
+            $verifications = verifChamps();
+            
+            if ($verifications['tout']) {
+                
+                $pdo = connexionBD();
+                
+                $nom = htmlspecialchars($_POST['nom']);
+                $prenom = htmlspecialchars($_POST['prenom']);
+                $telephone = htmlspecialchars($_POST['telephone']);
+                $login = htmlspecialchars($_POST['login']);
+                $mdp = htmlspecialchars($_POST['mdp']);
+                
+                ajoutEmploye($pdo, $nom, $prenom, $telephone, $login, $mdp);
+                $_SESSION['ajout_employe'] = true;
+                header('Location: consultationEmploye.php');
+            }
+        }
+        
+    } catch (Exception $e) {
+        echo $e;
+        //header('Location: erreurBD.php');
+    }
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -21,20 +52,41 @@
                                 <div class = "col-12 titre">
                                     <h1>Informations personnelles</h1>
                                 </div>
-                                <div class = "col-12">
+                                <?php
+                                    echo '<div class = "col-12';
+                                    if (isset ($verifications) && !$verifications['nom']) {
+                                        echo " rouge";
+                                    }
+                                    echo '">';
+                                ?>
                                     <!-- nom -->
-                                    <label for = "nom">Nom : </label><br>
-                                    <input type = "text" name = "nom" id = "nom" placeholder = "Entrez le nom de l'employé"><br><br>
+                                    <label for = "nom">Nom : <span class = "rouge">*</span></label><br>
+                                    <input type = "text" name = "nom" id = "nom" placeholder = "Entrez le nom de l'employé" 
+                                     <?php if (isset($_POST['nom'])) { echo 'value = "'.htmlspecialchars($_POST['nom']).'"'; } ?> ><br><br>
                                 </div>
-                                <div class = "col-12">
+                                <?php
+                                    echo '<div class = "col-12';
+                                    if (isset ($verifications) && !$verifications['prenom']) {
+                                        echo " rouge";
+                                    }
+                                    echo '">';
+                                ?>
                                     <!-- prénom -->
-                                    <label for = "prenom">Prénom : </label><br>
-                                    <input type = "text" name = "prenom" id = "prenom" placeholder = "Entrez le prénom de l'employé"><br><br>
+                                    <label for = "prenom">Prénom : <span class = "rouge">*</span></label><br>
+                                    <input type = "text" name = "prenom" id = "prenom" placeholder = "Entrez le prénom de l'employé"
+                                    <?php if (isset($_POST['prenom'])) { echo 'value = "'.htmlspecialchars($_POST['prenom']).'"'; } ?> ><br><br>
                                 </div>
-                                <div class = "col-12">
+                                <?php
+                                    echo '<div class = "col-12';
+                                    if (isset ($verifications) && !$verifications['telephone']) {
+                                        echo " rouge";
+                                    }
+                                    echo '">';
+                                ?>
                                     <!-- téléphone -->
                                     <label for = "telephone">Téléphone : </label><br>
-                                    <input type = "text" name = "telephone" id = "telephone" placeholder = "Entrez le téléphone de l'employé"><br><br>
+                                    <input type = "text" name = "telephone" id = "telephone" placeholder = "Entrez le téléphone de l'employé"
+                                    <?php if (isset($_POST['telephone'])) { echo 'value = "'.htmlspecialchars($_POST['telephone']).'"'; } ?> ><br><br>
                                 </div>
                             </div>
                         </div>
@@ -45,20 +97,41 @@
                                 <div class = "col-12 titre">
                                     <h1>Informations du compte</h1>
                                 </div>
-                                <div class = "col-12">
+                                <?php
+                                    echo '<div class = "col-12';
+                                    if (isset ($verifications) && !$verifications['login']) {
+                                        echo " rouge";
+                                    }
+                                    echo '">';
+                                ?>
                                     <!-- login -->
-                                    <label for = "login">Identifiant : </label><br>
-                                    <input type = "text" name = "login" id = "login" placeholder = "Entrez l'idenfiant du compte"><br><br>
+                                    <label for = "login">Identifiant : <span class = "rouge">*</span></label><br>
+                                    <input type = "text" name = "login" id = "login" placeholder = "Entrez l'idenfiant du compte"
+                                    <?php if (isset($_POST['login'])) { echo 'value = "'.htmlspecialchars($_POST['login']).'"'; } ?> ><br><br>
                                 </div>
-                                <div class = "col-12">
+                                <?php
+                                    echo '<div class = "col-12';
+                                    if (isset ($verifications) && !$verifications['mdp']) {
+                                        echo " rouge";
+                                    }
+                                    echo '">';
+                                ?>
                                     <!-- mot de passe -->
-                                    <label for = "mdp">Mot de Passe : </label><br>
-                                    <input type = "password" name = "mdp" id = "mdp" placeholder = "Entrez le mot de passe du compte"><br><br>
+                                    <label for = "mdp">Mot de Passe : <span class = "rouge">*</span></label><br>
+                                    <input type = "password" name = "mdp" id = "mdp" placeholder = "Entrez le mot de passe du compte"
+                                    <?php if (isset($_POST['mdp'])) { echo 'value = "'.htmlspecialchars($_POST['mdp']).'"'; } ?> ><br><br>
                                 </div>
-                                <div class = "col-12">
+                                <?php
+                                    echo '<div class = "col-12';
+                                    if (isset ($verifications) && !$verifications['confirm_mdp']) {
+                                        echo " rouge";
+                                    }
+                                    echo '">';
+                                ?>
                                     <!-- mot de passe -->
-                                    <label for = "confirm_mdp">Confirmation mot de Passe : </label><br>
-                                    <input type = "password" name = "confirm_mdp" id = "confirm_mdp" placeholder = "Confirmez le mot de passe du compte"><br><br>
+                                    <label for = "confirm_mdp">Confirmation mot de Passe : <span class = "rouge">*</span></label><br>
+                                    <input type = "password" name = "confirm_mdp" id = "confirm_mdp" placeholder = "Confirmez le mot de passe du compte"
+                                    <?php if (isset($_POST['confirm_mdp'])) { echo 'value = "'.htmlspecialchars($_POST['confirm_mdp']).'"'; } ?> ><br><br>
                                 </div>
                             </div>
                         </div>
