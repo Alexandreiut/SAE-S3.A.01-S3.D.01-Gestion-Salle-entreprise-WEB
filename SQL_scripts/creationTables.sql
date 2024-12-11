5be675106e35af4ef4c96afc8586bd8e0179ef39
Arcier Noé <168074918+NoeArcier@users.noreply.github.com>
2024-12-11 10:39:39 +0100

    Update creationTables.sql
    telephone not null

--- SQL_scripts/creationTables.sql
+++ SQL_scripts/creationTables.sql
@@ -44,7 +44,7 @@ CREATE TABLE `utilisateur` (
   `identifiant` int(7) NOT NULL,
   `nom` varchar(70) NOT NULL,
   `prenom` varchar(70) NOT NULL,
-  `telephone` char(4) NOT NULL,
+  `telephone` char(4),
   `role` varchar(70) NOT NULL,
   `login` varchar(70) NOT NULL,
   `motDePasse` varchar(70) NOT NULL
