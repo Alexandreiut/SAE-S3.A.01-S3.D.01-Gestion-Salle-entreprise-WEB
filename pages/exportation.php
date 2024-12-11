743aefa535854b7597a32fc64dd4a203d9cba72f
LUCAS BOULOUARD <lucas.boulouard@iut.local>
2024-12-11 14:27:36 +0100

    mise en place de la responsivité sur exportation.php

--- pages/exportation.php
+++ pages/exportation.php
@@ -3,46 +3,56 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../ressources/fontawesome-free-6.5.1-web/css/all.min.css"> <!-- Lien vers Font Awesome -->
-        <link rel="stylesheet" href="../ressources/boostrap-5.3.2-dist/css/boostrap.min.css">
+        <link rel="stylesheet" href="../ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/exportationCss.css">
     </head>
     <body>
-        <div class="header">
+        <div class="header text-center mb-4">
             <img src="../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
             <h1>Exportation</h1>
         </div>
+    
+        <div class="container">
+            <!-- Ligne 1 : Deux boutons en haut -->
+            <div class="row justify-content-center mb-4">
+                <div class="col-12 col-md-5 text-center mb-3 mb-md-0">
+                    <button class="btn-export w-100">
+                        <i class="fas fa-chalkboard-teacher"></i> Activités
+                    </button>
+                </div>
+                <div class="col-12 col-md-5 text-center">
+                    <button class="btn-export w-100">
+                        <i class="fas fa-user"></i> Employés
+                    </button>
+                </div>
+            </div>
 
-    <div class="container">
-        <!-- Ligne 1 : Deux boutons en haut -->
-        <div class="row">
-            <button class="btn-export">
-                <i class="fas fa-chalkboard-teacher"></i> Activités
-            </button>
-            <button class="btn-export">
-                <i class="fas fa-user"></i> Employés
-            </button>
+            <!-- Ligne 2 : Deux boutons au milieu -->
+            <div class="row justify-content-center mb-4">
+                <div class="col-12 col-md-5 text-center mb-3 mb-md-0">
+                    <button class="btn-export w-100">
+                        <i class="fas fa-clock-rotate-left"></i> Réservations
+                    </button> 
+                </div>
+                <div class="col-12 col-md-5 text-center">
+                    <button class="btn-export w-100">
+                        <i class="fas fa-door-open"></i> Salles
+                    </button>
+                </div>
+            </div>
+
+            <!-- Ligne 3 : Un bouton centré tout en bas -->
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6 text-center">
+                    <button class="btn-export w-100">
+                        <i class="fas fa-download"></i> Tout Exporter
+                    </button>
+                </div>
+            </div>
         </div>
 
-        <!-- Ligne 2 : Deux boutons au milieu -->
-        <div class="row">
-            <button class="btn-export">
-                <i class="fas fa-clock-rotate-left"></i></i> Réservations
-            </button>
-            <button class="btn-export">
-                <i class="fas fa-door-open"></i> Salles
-            </button>
-        </div>
-
-        <!-- Ligne 3 : Un bouton centré tout en bas -->
-        <div class="row">
-            <button class="btn-export">
-                <i class="fas fa-download"></i> Tout Exporter
-            </button>
-        </div>
-    </div>
-
-    <footer class="footer">
-        © 2024 Room Manager | Assistance : support@roommanager.com
-    </footer>
+        <footer class="footer text-center mt-4">
+            © 2024 Room Manager | Assistance : support@roommanager.com
+        </footer>
     </body>
 </html>
