f5afb9b8fe32a199659699a7d0588ea6fd9c4556
LUCAS BOULOUARD <lucas.boulouard@iut.local>
2024-12-12 11:49:00 +0100

    mise en place d'aide à l'utilisateur (message d'alert avec js + message hover) sur la page exportation

--- pages/exportation.php
+++ pages/exportation.php
@@ -5,6 +5,8 @@
         <link rel="stylesheet" href="../ressources/fontawesome-free-6.5.1-web/css/all.min.css"> <!-- Lien vers Font Awesome -->
         <link rel="stylesheet" href="../ressources/bootstrap-5.3.2-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/exportationCss.css">
+        <script src="../js/exportation.js" defer></script>
+
     </head>
     <body>
         <div class="header text-center mb-4">
@@ -16,12 +18,12 @@
             <!-- Ligne 1 : Deux boutons en haut -->
             <div class="row justify-content-center mb-4">
                 <div class="col-12 col-md-5 text-center mb-3 mb-md-0">
-                    <button class="btn-export w-100">
+                    <button class="btn-export w-100" title="Exporter toutes les données des activités">
                         <i class="fas fa-chalkboard-teacher"></i> Activités
                     </button>
                 </div>
                 <div class="col-12 col-md-5 text-center">
-                    <button class="btn-export w-100">
+                    <button class="btn-export w-100" title="Exporter toutes les données des employés">
                         <i class="fas fa-user"></i> Employés
                     </button>
                 </div>
@@ -30,12 +32,12 @@
             <!-- Ligne 2 : Deux boutons au milieu -->
             <div class="row justify-content-center mb-4">
                 <div class="col-12 col-md-5 text-center mb-3 mb-md-0">
-                    <button class="btn-export w-100">
+                    <button class="btn-export w-100" title="Exporter toutes les données des réservations">
                         <i class="fas fa-clock-rotate-left"></i> Réservations
                     </button> 
                 </div>
                 <div class="col-12 col-md-5 text-center">
-                    <button class="btn-export w-100">
+                    <button class="btn-export w-100" title="Exporter toutes les données des salles">
                         <i class="fas fa-door-open"></i> Salles
                     </button>
                 </div>
@@ -44,7 +46,7 @@
             <!-- Ligne 3 : Un bouton centré tout en bas -->
             <div class="row justify-content-center">
                 <div class="col-12 col-md- text-center">
-                    <button class="btn-export w-100">
+                    <button class="btn-export w-100" title="Exporter toutes les données disponibles" onclick="confirmExport()"> <!-- Ajout du onclick -->
                         <i class="fas fa-download"></i> Tout Exporter
                     </button>
                 </div>
