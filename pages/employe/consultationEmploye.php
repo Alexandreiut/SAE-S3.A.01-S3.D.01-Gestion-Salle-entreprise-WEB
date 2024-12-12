c042bfd5b978925140a18e95ae067d095d1bc8dc
NOE ARCIER <noe.arcier@iut.local>
2024-12-12 11:59:54 +0100

    frontend consultation

--- pages/employe/consultationEmploye.php
+++ pages/employe/consultationEmploye.php
@@ -8,26 +8,40 @@
         <link rel="stylesheet" href="../../css/consultations.css" />
     </head>
     <body>
-        <!-- Barre de navigation -->
         <div class="header">
-            <!-- Section gauche -->
             <div class="header-left">
-                <i class="fa-solid fa-bars menu" alt="menu" id="menu"></i>
-                <img src="../../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
+				<div class="menu-container">
+					<button type="submit" class="bouton-menu" id="menuButton"><i class="fas fa-bars menu" id="menuIcon"></i></button>
+				</div>
+				<img src="../../ressources/image/LogoRoomManagerReservation.png" alt="Logo" class="logo">
             </div>
 
-            <!-- Titre au centre -->
             <div class="header-title">
-                <i class="fa-solid fa-user"></i>
-                <h1>Employé</h1>
+                <h1>RoomManager</h1>
             </div>
 
-            <!-- Bouton à droite -->
-            <button type="submit" class="deconnexion">
-                Se déconnecter
-                <i class="fa-solid fa-right-from-bracket"></i>
-            </button>
+			<form method="post" action="../accueilEmploye.php">
+				<input type="hidden" name="deconnexion" id="deconnexion" value="1">
+				<button type="submit" class="deconnexion">
+					Se déconnecter
+					<i class="fas fa-right-from-bracket"></i>
+				</button>
+			</form>
         </div>
 
+		<!--Initialement caché-->
+		<div id="sideMenu" class="side-menu">
+			<ul>
+				<li><a href="#"><i class="fas fa-house"></i> Accueil</a></li>
+				<li><a href="#"><i class="fas fa-clock-rotate-left"></i> Réservation</a></li>
+				<li><a href="#"><i class="fas fa-door-open"></i> Salle</a></li>
+				<li><a href="#"><i class="fas fa-download"></i> Télécharger</a></li>
+			</ul>
+		</div>
+        
+        <div class="footer">
+			<p>2024 © RoomManager. IUT de Rodez.</p>
+		</div>
+        <script src="../../engine/js/menu.js" defer></script>
     </body>
 </html>
