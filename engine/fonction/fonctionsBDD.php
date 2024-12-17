63c5b641cbdd8d323e55871de54f19ba03f5ec84
NOE ARCIER <noe.arcier@iut.local>
2024-12-17 15:34:18 +0100

    merge fonctionsBDDfunction ajoutEmploye($pdo, $nom, $prenom, $telephone, $login, $mdp) {

--- engine/fonction/fonctionsBDD.php
+++ engine/fonction/fonctionsBDD.php
@@ -1,22 +1,65 @@
 <?php
-    function connexionBD() {
-        $host='localhost';
-        $db='roommanager';
-        $user='root';
-        $pass='root';
-        $charset='utf8mb4';
-        
+    function connexion($db) {
+        $host='localhost'; //Modif
+        $user='root'; // Modif
+        $pass='root'; //Modif
+        $charset='utf8mb4'; 
+    
         $dsn="mysql:host=$host;dbname=$db;charset=$charset";
+    
+        $options=[
+        PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES=>false];
         
-        $options=[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
-                  PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
-                  PDO::ATTR_EMULATE_PREPARES=>false];
-        
-        $pdo=new PDO($dsn,$user,$pass,$options);
-        
+        try {
+            $pdo=new PDO($dsn,$user,$pass,$options);
+        } catch(PDOException $connexionErreur){
+            throw new PDOException($connexionErreur->getMessage(), (int)$connexionErreur->getCode());
+        }
         return $pdo;
     }
-    
+
+    function authentification($pdo, $login = "", $pwd = ""){
+		$requete = "SELECT COUNT(*) FROM utilisateur WHERE login = :login";
+		
+		$stmt = $pdo->prepare($requete);
+		$stmt->bindParam(':login', $login);
+		
+		$stmt->execute();
+
+        $tabBoolean = [];
+        $tabBoolean[0] = $stmt->fetch()['COUNT(*)'] != 0;
+
+        $requete = "SELECT COUNT(*) FROM utilisateur WHERE login = :login AND motDePasse = md5(:pwd)";
+        $stmt = $pdo->prepare($requete);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':pwd', $pwd);
+        $stmt->execute();
+
+        $tabBoolean[1] = $stmt->fetch()['COUNT(*)'] != 0;
+		
+		return $tabBoolean;
+	}
+
+    function deconnexion() {
+		session_destroy();
+		header('Location: ../index.php');
+		exit();
+	}
+
+    function getRole($pdo, $login, $pwd){
+        $requete = "SELECT role FROM utilisateur WHERE login = :login AND motDePasse = md5(:pwd)";
+        
+        $stmt = $pdo->prepare($requete);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':pwd', $pwd);
+        
+        $stmt->execute();
+        
+        return $stmt->fetch()["role"];
+    }
+
     function ajoutEmploye($pdo, $nom, $prenom, $telephone, $login, $mdp) {
         
         $requete = "INSERT INTO utilisateur(nom, prenom, telephone, role, login, motDePasse)
--- engine/fonctionsAuthentification.php
+++ engine/fonctionsAuthentification.php
@@ -1,7 +1,6 @@
 <?php
-    function connexion($bd) {
+    function connexion($db) {
         $host='localhost'; //Modif
-        $db= $bd;
         $user='root'; // Modif
         $pass='root'; //Modif
         $charset='utf8mb4'; 
@@ -59,5 +58,77 @@
         $stmt->execute();
         
         return $stmt->fetch()["role"];
+    }
+
+    function ajoutEmploye($pdo, $nom, $prenom, $telephone, $login, $mdp) {
+        
+        $requete = "INSERT INTO utilisateur(nom, prenom, telephone, role, login, motDePasse)
+                    VALUES (:nom, :prenom, NULLIF(:telephone, ''), 'employe', :login, md5(:motDePasse))";
+        
+        $stmt = $pdo->prepare($requete);
+        
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':telephone', $telephone);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':motDePasse', $mdp);
+        
+        $stmt->execute();
+    }
+    
+    function verifChamps() {
+        
+        $verifications = [];
+        
+        if (isset($_POST['telephone']) && $_POST['telephone'] != "" ) {
+            
+            // vérifie que le téléphone soit un entier
+            try {
+                intval($_POST['telephone']);
+                $estEntier = true;
+            } catch (Exception $e) {
+                $estEntier = false;
+            }
+        }
+        
+        $verifications['nom'] = isset($_POST['nom']) && $_POST['nom'] != "";
+        $verifications['prenom'] = isset($_POST['prenom']) && $_POST['prenom'] != "";
+        $verifications['telephone'] = isset($_POST['telephone']) 
+                                      && ($_POST['telephone'] == ""
+                                          || strlen($_POST['telephone']) == 4
+                                          && $estEntier);
+        $verifications['login'] = isset($_POST['login']) && $_POST['login'] != "";
+        $verifications['mdp'] = isset($_POST['mdp']) && $_POST['mdp'] != "";
+        $verifications['confirm_mdp'] = isset($_POST['confirm_mdp']) && $_POST['confirm_mdp'] != "" && $_POST['confirm_mdp'] == $_POST['mdp'];
+        
+        $verifications['tout'] = true;
+        foreach ($verifications as $verif) {
+            $verifications['tout'] &= $verif;
+        }
+        
+        return $verifications;
+    }
+    
+    function modifEmploye($pdo, $id, $nom, $prenom, $telephone, $login, $mdp) {
+        
+        $requete = "UPDATE utilisateur
+                    SET
+                        nom = :nom,
+                        prenom = :prenom,
+                        telephone = NULLIF(:telephone, ''),
+                        login = :login,
+                        motDePasse = md5(:motDePasse)
+                    WHERE identifiant = :id";
+        
+        $stmt = $pdo->prepare($requete);
+        
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':telephone', $telephone);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':motDePasse', $mdp);
+        $stmt->bindParam(':id', $id);
+        
+        $stmt->execute();
     }
 ?>
