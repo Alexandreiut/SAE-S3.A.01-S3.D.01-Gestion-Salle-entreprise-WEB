30f7e8c93e97cb4af49d4fc8c6d36f7342aa3fab
SamuelLacam <s.lacam46270@gmail.com>
2024-12-18 09:22:01 +0100

    affichage dynamique des employés

--- engine/affichage/consultation.php
+++ engine/affichage/consultation.php
@@ -1,21 +1,27 @@
 <?php
     function affichageEmployes($employes) {
+        $lien = "../../pages/employe/formulaireEmploye.php";
+
         foreach ($employes as $e) {
-            echo '<div class="row mt-1">
+            echo '<div class="row mt-1 mb-1">
                     <div class="sous-container-informations">
                         <span class="col-lg-4 texte-ellipsis">' . $e["prenom"] . " " . $e["nom"] . '</span>
-                        <div class="col-lg-2 btn-details-modifier">
-                            <i class="fa-solid fa-circle-info taille-icon-infos"></i>
-                            Voir les détails
-                        </div>
-                        <div class="col-lg-2 btn-details-modifier">
-                            <i class="fa-solid fa-pen-to-square taille-icon-infos"></i>
-                            Modifier
-                        </div>
-                        <div class="col-lg-2 btn-supprimer">
+                        <form class="col-lg-2 form-btn-items" action="' . $lien . '" method="post">
+                            <button class="btn-details-modifier" name="action" value="détails" type="submit">
+                                <i class="fa-solid fa-circle-info taille-icon-infos"></i>
+                                Voir les détails
+                            </button>
+                        </form>
+                        <form class="col-lg-2 form-btn-items" action="' . $lien . '" method="post">
+                            <button class="btn-details-modifier" name="action" value="modifier" type="submit">
+                                <i class="fa-solid fa-pen-to-square taille-icon-infos"></i>
+                                Modifier
+                            </button>
+                        </form>
+                        <button class="col-lg-2 btn-supprimer">
                             <i class="fa-solid fa-trash-can taille-icon-infos"></i>
                             Supprimer
-                        </div>
+                        </button>
                     </div>
                 </div>';
         }
