dac0bef190d28a6a4e3d1f64b84d8d170e64fff7
SamuelLacam <s.lacam46270@gmail.com>
2025-01-07 15:07:59 +0100

    ajout de l'impossibilité d'accéder à la page de formulaire employé sans être admin

--- pages/employe/formulaireEmploye.php
+++ pages/employe/formulaireEmploye.php
@@ -9,6 +9,12 @@
 	
 	$role = $_SESSION['role'];
 
+    //si l'utilisateur n'est pas administrateur
+    if ($role != "administrateur") {
+        header('Location: ../../index.php');
+        exit();
+    }
+
 	if(isset($_POST['deconnexion']) && $_POST['deconnexion'] == '1'){
 		session_destroy();
 		header('Location: ../../index.php');
